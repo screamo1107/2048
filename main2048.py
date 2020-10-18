eef4bc1e8dd76f77d1960206c55444369e6ecfdd
screamo1107 <screamo1107@gmail.com>
2020-10-18 12:04:56 +0200

    Leaderboard implementation - initial

--- main2048.py
+++ main2048.py
@@ -3,6 +3,7 @@ import pygame
 import random
 import sys
 from game_logic import *
+from leaderboard import crsr, get_best_results
 
 
 # Initial matrix
@@ -13,24 +14,44 @@ mas = [
         [0, 0, 0, 0]
       ]
 
+GAMERS_DATA = get_best_results()
+
+
 # Game-board initialize
 pygame.init()
 screen = pygame.display.set_mode((c.WIDTH, c.HEIGHT))
 pygame.display.set_caption(c.HEADER_TEXT)
 
 
+def draw_best_results():
+    font_best_header = pygame.font.SysFont(c.SCORE_FONT, c.BEST_HEADER_SIZE)
+    font_best_player = pygame.font.SysFont(c.SCORE_FONT, c.BEST_PLAYERS_SIZE)
+    text_best_header = font_best_header.render("Best results: ", True, c.COLORS['SCORE'])
+    screen.blit(text_best_header, (300, 5))
+    for index, player in enumerate(GAMERS_DATA):
+        name, score = player
+        r = f"{index+1}. {name} - {score}"
+        text_best_player = font_best_player.render(r, True, c.COLORS['SCORE'])
+        screen.blit(text_best_player, (300, 35 + 25 * index))
+
+
 def draw_interface(score, delta=0):
     font_digit = pygame.font.SysFont(c.DIGIT_FONT, c.DIGIT_SIZE)
     font_score = pygame.font.SysFont(c.SCORE_FONT, c.SCORE_SIZE)
-    font_delta = pygame.font.SysFont(c.SCORE_FONT, c.DELTA_SIZE)
     text_score = font_score.render("Your score: ", True, c.COLORS['SCORE'])
     score_value = font_score.render(f"{score}", True, c.COLORS['SCORE'])
+    font_delta = pygame.font.SysFont(c.SCORE_FONT, c.DELTA_SIZE)
+
     pygame.draw.rect(screen, c.COLORS['HEADER'], c.TITLE_REC)
     screen.blit(text_score, (20, 35))
     screen.blit(score_value, (220, 35))
+
+    # Drawing +score dynamically
     if delta > 0:
         delta_value = font_delta.render(f"+{delta}!", True, c.COLORS['SCORE'])
         screen.blit(delta_value, (222, 70))
+
+    draw_best_results()  # Displaying of score-board
     for row in range(c.BLOCKS):
         for column in range(c.BLOCKS):
             value = mas[row][column]
@@ -38,6 +59,7 @@ def draw_interface(score, delta=0):
             w = column * c.BLOCK_SIZE + (column + 1) * c.MARGIN
             h = row * c.BLOCK_SIZE + (row + 1) * c.MARGIN + c.BLOCK_SIZE
             pygame.draw.rect(screen, c.COLORS[str(value)], (w, h, c.BLOCK_SIZE, c.BLOCK_SIZE))
+
             # Draw digit inside the cell
             if value != 0:
                 font_w, font_h = text.get_size()
@@ -52,6 +74,10 @@ draw_interface(score)
 pygame.display.update()
 
 
+def draw_intro_screen():
+    pass
+
+
 # Main game-cycle
 while is_zero_in_mas(mas) or can_move(mas):
     for event in pygame.event.get():
@@ -61,6 +87,7 @@ while is_zero_in_mas(mas) or can_move(mas):
             sys.exit(0)
         elif event.type == pygame.KEYDOWN:
             delta = 0
+
             # Keys handling
             if event.key == pygame.K_LEFT:
                 mas, delta = move_left(mas)
