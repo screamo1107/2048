fd73ba6e92fe7db522c00b7bf30c1e5ee31b85a7
screamo1107 <screamo1107@gmail.com>
2020-10-18 14:59:07 +0200

    Major refactoring: colors/constants

--- constants.py
+++ constants.py
@@ -1,29 +1,39 @@
 import pygame
 
 
+# Screen:
 BLOCKS = 4
 BLOCK_SIZE = 120
-BEST_HEADER_SIZE = 35
-BEST_PLAYERS_SIZE = 30
-DELTA_SIZE = 30
-DIGIT_SIZE = 65
+HEADER_TEXT = "Let's play 2048!"
 MARGIN = 5
-SCORE_SIZE = 45
 
 WIDTH = BLOCKS * BLOCK_SIZE + (BLOCKS + 1) * MARGIN
 HEIGHT = WIDTH + BLOCK_SIZE
 TITLE_REC = pygame.Rect(0, 0, WIDTH, BLOCK_SIZE)
 
 
-DIGIT_FONT = "arial"
-HEADER_TEXT = "Let's play 2048!"
-SCORE_FONT = "simsun"
+# Font sizes:
+BEST_HEADER_SIZE = 35
+BEST_PLAYERS_SIZE = 30
+DELTA_SIZE = 30
+DIGIT_SIZE = 65
+INTRO_SIZE = 75
+SCORE_SIZE = 45
 
 
+# Fonts:
+DIGIT_FONT = 'arial'
+INTRO_FONT = 'allegro'
+SCORE_FONT = 'simsun'
+
+
+# Colors:
 COLORS = {
-         "HEADER": (255, 255, 255),
-         'DIGIT': (0, 0, 0),
-         'SCORE': (150, 120, 100),
+         'BLACK': (0, 0, 0),
+         'DELTA': (100, 0, 0),
+         'SCORE_TEXT': (160, 100, 0),
+         'SCORE_VALUE': (170, 0, 0),
+         'WHITE': (255, 255, 255),
          '0': (190, 190, 190),
          '2': (255, 255, 255),
          '4': (255, 255, 135),
--- main2048.py
+++ main2048.py
@@ -14,8 +14,8 @@ mas = [
         [0, 0, 0, 0]
       ]
 
-USERNAME = None
 GAMERS_DATA = get_best_results()
+USERNAME = None
 
 
 # Game-board initialize
@@ -26,37 +26,38 @@ pygame.display.set_caption(c.HEADER_TEXT)
 
 def draw_best_results():
     font_best_header = pygame.font.SysFont(c.SCORE_FONT, c.BEST_HEADER_SIZE)
+    text_best_header = font_best_header.render("Best results: ", True, c.COLORS['BLACK'])
     font_best_player = pygame.font.SysFont(c.SCORE_FONT, c.BEST_PLAYERS_SIZE)
-    text_best_header = font_best_header.render("Best results: ", True, c.COLORS['SCORE'])
-    screen.blit(text_best_header, (300, 5))
+    screen.blit(text_best_header, (330, 5))
     for index, player in enumerate(GAMERS_DATA):
-        name, score = player
-        r = f"{index+1}. {name} - {score}"
-        text_best_player = font_best_player.render(r, True, c.COLORS['SCORE'])
-        screen.blit(text_best_player, (300, 35 + 25 * index))
+        name, pl_score = player
+        r = f"{index+1}. {name} - {pl_score}"
+        text_best_player = font_best_player.render(r, True, c.COLORS['BLACK'])
+        screen.blit(text_best_player, (330, 35 + 25 * index))
 
 
-def draw_interface(score, delta=0):
+def draw_interface(score: int, delta: int = 0):
     font_digit = pygame.font.SysFont(c.DIGIT_FONT, c.DIGIT_SIZE)
     font_score = pygame.font.SysFont(c.SCORE_FONT, c.SCORE_SIZE)
-    text_score = font_score.render("Your score: ", True, c.COLORS['SCORE'])
-    score_value = font_score.render(f"{score}", True, c.COLORS['SCORE'])
+    text_score = font_score.render("Your score: ", True, c.COLORS['BLACK'])
+    score_value = font_score.render(f"{score}", True, c.COLORS['SCORE_VALUE'])
     font_delta = pygame.font.SysFont(c.SCORE_FONT, c.DELTA_SIZE)
 
-    pygame.draw.rect(screen, c.COLORS['HEADER'], c.TITLE_REC)
+    # Drawing header with score text and value
+    pygame.draw.rect(screen, c.COLORS['WHITE'], c.TITLE_REC)
     screen.blit(text_score, (20, 35))
-    screen.blit(score_value, (220, 35))
+    screen.blit(score_value, (215, 37))
 
-    # Drawing +score dynamically
+    # Drawing +score update dynamically
     if delta > 0:
-        delta_value = font_delta.render(f"+{delta}!", True, c.COLORS['SCORE'])
-        screen.blit(delta_value, (222, 70))
+        delta_value = font_delta.render(f"+{delta}!", True, c.COLORS['DELTA'])
+        screen.blit(delta_value, (221, 70))
 
-    draw_best_results()  # Displaying of score-board
+    draw_best_results()  # Trigger displaying of best players score-board
     for row in range(c.BLOCKS):
         for column in range(c.BLOCKS):
             value = mas[row][column]
-            text = font_digit.render(str(value), True, c.COLORS['DIGIT'])
+            text = font_digit.render(str(value), True, c.COLORS['BLACK'])
             w = column * c.BLOCK_SIZE + (column + 1) * c.MARGIN
             h = row * c.BLOCK_SIZE + (row + 1) * c.MARGIN + c.BLOCK_SIZE
             pygame.draw.rect(screen, c.COLORS[str(value)], (w, h, c.BLOCK_SIZE, c.BLOCK_SIZE))
@@ -71,11 +72,12 @@ def draw_interface(score, delta=0):
 
 def draw_intro_screen():
     start_img = pygame.image.load('bg.png')
-    font_intro = pygame.font.SysFont(c.SCORE_FONT, c.SCORE_SIZE)
-    text_intro = font_intro.render("WELCOME!", True, c.COLORS['SCORE'])
+    font_intro = pygame.font.SysFont(c.INTRO_FONT, c.INTRO_SIZE)
+    text_intro = font_intro.render("WELCOME!", True, c.COLORS['WHITE'])
 
     name_input = 'Enter your name!'
     is_name_found = False
+    # Looping until correct name entered
     while not is_name_found:
         for event in pygame.event.get():
             # Close action handling
@@ -91,27 +93,28 @@ def draw_intro_screen():
                 elif event.key == pygame.K_BACKSPACE:
                     name_input = name_input[:-1]
                 elif event.key == pygame.K_RETURN:
-                    if len(name_input) > 2:
+                    if len(name_input) > 2 and name_input != "Enter your name!":
                         global USERNAME
                         USERNAME = name_input
                         is_name_found = True
                         break
+        screen.fill(c.COLORS['BLACK'])
 
-        screen.fill(c.COLORS['DIGIT'])
-
-        text_name = font_intro.render(name_input, True, c.COLORS['SCORE'])
+        # Rendering intro background image, text and username
+        text_name = font_intro.render(name_input, True, c.COLORS['WHITE'])
         rect_name = text_name.get_rect()
         rect_name.center = screen.get_rect().center
         screen.blit(pygame.transform.scale(start_img, [200, 200]), [10, 10])
-        screen.blit(text_intro, (200, 230))
+        screen.blit(text_intro, (110, 230))
         screen.blit(text_name, rect_name)
         pygame.display.update()
-    screen.fill(c.COLORS['DIGIT'])
+    screen.fill(c.COLORS['BLACK'])
 
 
+# Draw intro screen firstly
 draw_intro_screen()
 
-# Draw initial game-board
+# Draw initial game-board once intro is passed
 score = 0
 draw_interface(score)
 pygame.display.update()
